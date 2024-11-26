b68ae1bf51769f5de8a55e15d0499d840966534d
dontpanicw <valerycecet8@gmail.com>
2024-11-26 23:54:01 +0300

    Delete Backend

--- Backend/api/start.py
+++
@@ -1,4 +0,0 @@
-from main import app
-import uvicorn
-
-uvicorn.run(app, port = 8000, host='0.0.0.0')
--- Backend/db/settings.py
+++
@@ -1,9 +0,0 @@
-from envparse import Env
-
-env = Env()
-
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/itam_project"
-
-SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
-ALGORITHM: str = env.str("ALGORITHM", default="HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
